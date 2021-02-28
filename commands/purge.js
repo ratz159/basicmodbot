d0888dfbe141df97ef96ed1a4263495b31b4a210
Scott Pendland <smpendland@outlook.com>
2021-02-28 12:42:41 -0500

    Fixed line break issue

--- commands/purge.js
+++ commands/purge.js
@@ -8,8 +8,7 @@ module.exports = {
         if (!(executioner.hasPermission('MANAGE_MESSAGES'))) return;
         msg.delete().then( () => {
             const messagecount = parseInt(args[0]);
-            msg.channel.messages.fetch({ limit: messagecount })
-                .then(messages => msg.channel.bulkDelete(messages));
+            msg.channel.messages.fetch({ limit: messagecount }).then(messages => msg.channel.bulkDelete(messages));
         }).then(() => {
             msg.channel.send(`Deleted ${ args[0] } messages. (This message deletes after 5 seconds).`).then(msg => msg.delete( {timeout: 5000 } ));
         })
